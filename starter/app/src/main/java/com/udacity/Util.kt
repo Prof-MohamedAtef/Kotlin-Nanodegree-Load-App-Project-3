28adc690ca203149d4357f03d85ec04487bdca04
Mohamed Atef <proofessergmail.com>
2022-10-18 22:36:08 +0100

    added Util

---
+++ starter/app/src/main/java/com/udacity/Util.kt
@@ -0,0 +1,10 @@
+package com.udacity
+
+class Util {
+    companion object{
+        const val URL_APP =
+            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        const val URL_GLIDE = "https://github.com/bumptech/glide"
+        const val URL_RETROFIT = "https://github.com/square/retrofit"
+    }
+}
